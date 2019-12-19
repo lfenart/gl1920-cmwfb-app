390778b3d090121561340861523a89c66cdeaa0f
Lucas Fénart <lucas.fenart@gmail.com>
2019-12-19 13:35:02 +0100

    Add mockito tests

---
+++ src/test/java/fr/uha/ensisa/gl/cmwfb/controller/TestSerieController.java
@@ -0,0 +1,14 @@
+package fr.uha.ensisa.gl.cmwfb.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSerieController {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
