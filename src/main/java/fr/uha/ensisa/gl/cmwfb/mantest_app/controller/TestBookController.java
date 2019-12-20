3a646b0592af9b68fb03f7f8df513e4a2992b400
Lucas Fénart <lucas.fenart@gmail.com>
2019-12-20 17:59:15 +0100

    Add unit tests for TestBookController

--- src/main/java/fr/uha/ensisa/gl/cmwfb/mantest_app/controller/TestBookController.java
+++ src/main/java/fr/uha/ensisa/gl/cmwfb/mantest_app/controller/TestBookController.java
@@ -11,37 +11,39 @@ import fr.uha.ensisa.gl.cmwfb.mantest.dao.DaoFactory;
 
 @Controller
 public class TestBookController {
-	
+
 	@Autowired
 	public DaoFactory daoFactory;
-	
-	 @RequestMapping(value = "/books")
-		public ModelAndView books() {
-			ModelAndView ret = new ModelAndView("books");
-			// Adds an objet to be used in home.jsp
-			ret.addObject("testBooks", daoFactory.getTestBookDao().findAll());
-			return ret;
-		}
-		
-		@RequestMapping(value = "/createBook")
-		public String createBook(@RequestParam(required = true) String testBookName) {
-			TestBook newTestBook = new TestBook();
-			long id = daoFactory.getTestBookDao().count() + 1;
-			while (daoFactory.getTestBookDao().find(id) != null)
-				id++;
-			newTestBook.setId(id);
-			newTestBook.setName(testBookName);
-			daoFactory.getTestBookDao().persist(newTestBook);
-			return "redirect:/books";
-		}
-		
-		/*@RequestMapping(value = "/book")
-		public ModelAndView book(@RequestParam(required = true) long testBookId) throws IOException {
-			ModelAndView ret = new ModelAndView("book");
-			ret.addObject("testBook", daoFactory.getTestBookDao().find(testBookId));
-			ret.addObject("tests", daoFactory.getTestBookDao().find(testBookId).getTests());
-			ret.addObject("state", daoFactory.getTestBookDao().find(testBookId).getState());
 
-			return ret;
-		}*/
+	@RequestMapping(value = "/books")
+	public ModelAndView books() {
+		ModelAndView ret = new ModelAndView("books");
+		// Adds an objet to be used in home.jsp
+		ret.addObject("testBooks", daoFactory.getTestBookDao().findAll());
+		return ret;
+	}
+
+	@RequestMapping(value = "/createBook")
+	public String createBook(@RequestParam(required = true) String testBookName) {
+		TestBook newTestBook = new TestBook();
+		long id = daoFactory.getTestBookDao().count() + 1;
+		while (daoFactory.getTestBookDao().find(id) != null)
+			id++;
+		newTestBook.setId(id);
+		newTestBook.setName(testBookName);
+		daoFactory.getTestBookDao().persist(newTestBook);
+		return "redirect:/books";
+	}
+
+	/*
+	 * @RequestMapping(value = "/book") public ModelAndView
+	 * book(@RequestParam(required = true) long testBookId) throws IOException {
+	 * ModelAndView ret = new ModelAndView("book"); ret.addObject("testBook",
+	 * daoFactory.getTestBookDao().find(testBookId)); ret.addObject("tests",
+	 * daoFactory.getTestBookDao().find(testBookId).getTests());
+	 * ret.addObject("state",
+	 * daoFactory.getTestBookDao().find(testBookId).getState());
+	 * 
+	 * return ret; }
+	 */
 }
